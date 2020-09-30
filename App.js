45ec26f32b180b4086ae113bfbaf3aa1abc8b91d
Veselin <vesselin60@gmail.com>
2020-09-30 10:44:18 +0300

    React Native’s Core Components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 
 const getFullName = (firstName, secondName, thirdName) => {
   return `${firstName} ${secondName} ${thirdName}`;
@@ -7,9 +7,17 @@ const getFullName = (firstName, secondName, thirdName) => {
 
 const Cat = () => {
   return (
-    <Text>
-      Hello, I am your cat {getFullName('Gocho', 'Kochev', 'The Prophet')}!
-    </Text>
+    <View>
+      <Text>Hello, I am ...</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'red',
+          borderWidth: 2,
+        }}
+        defaultValue="Give me a name!"
+      />
+    </View>
   );
 };
 
