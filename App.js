b06c0b04ce04f0abdb8a92c77c642c2ef723322e
Veselin <vesselin60@gmail.com>
2020-09-30 17:12:16 +0300

    A performant interface for rendering basic, flat lists, supporting the most handy features

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import {StyleSheet, Text, ScrollView, SafeAreaView} from 'react-native';
-//import Constants from 'expo-constants';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'First item',
+  },
+  {
+    id: '2',
+    title: 'Second item',
+  },
+  {
+    id: '3',
+    title: 'Third item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const App = () => {
+  const renderItem = ({item}) => <Item title={item.title} />;
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.ScrollView}>
-        <Text style={styles.text}>
-          This equipment has been tested and found to comply with the limits for
-          a Class B digital device, pursuant to part 15 of the FCC Rules. These
-          limits are designed to provide reasonable protection against harmful
-          interference in a residential installation. This equipment generates,
-          uses and can radiate radio frequency energy and, if not installed and
-          used in accordance with the instructions, may cause harmful
-          interference to radio communications. However, there is no guarantee
-          that interference will not occur in a particular installation. If this
-          equipment does cause harmful interference to radio or television
-          reception, which can be determined by turning the equipment off and
-          on, the user is encouraged to try to correct the interference by one
-          or more of the following measures:
-        </Text>
-      </ScrollView>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 };
@@ -28,14 +46,16 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //marginTop: Constants.statusBarHeight,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  ScrollView: {
-    backgroundColor: 'skyblue',
-    marginHorizontal: 20,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  text: {
-    fontSize: 42,
+  title: {
+    fontSize: 32,
   },
 });
 
