816fe9711d35c95f855469482e66f7e89ae4c26e
Veselin <vesselin60@gmail.com>
2020-09-30 10:47:14 +0300

    minor change

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {Text, TextInput, View} from 'react-native';
 
-const getFullName = (firstName, secondName, thirdName) => {
-  return `${firstName} ${secondName} ${thirdName}`;
-};
-
 const Cat = () => {
   return (
     <View>
